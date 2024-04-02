392b6096ebc377c76aceda18640ddb8061bdde8c
Jiaaming <2455951489@qq.com>
2024-04-02 11:32:48 +0800

    finished

--- echo_persona/pages/Persona_Chat.py
+++ echo_persona/pages/Persona_Chat.py
@@ -18,24 +18,15 @@ openai_api_key = st.session_state.openai_api_key
 st.subheader('Persona Q&A')
 st.markdown(
 """
-本交互模块是我们社交媒体用户画像分析系统的核心组成部分，旨在为使用者提供一个直观、高效的工具，通过对话交互形式探索特定用户的多维度画像。
-该模块允许使用者深入了解用户的性格特征、心理状态、兴趣爱好等，从而获得细致丰富的用户洞见。
-
-
 ##### 功能概述
-- **个性化查询**：使用者可以基于用户的特定维度（如心理状态、兴趣爱好）提出查询，系统将提供相关的详细信息。
-- **深度探索**：针对生成的用户画像，使用者可进行深入的问询，系统能够根据之前的交互历史和用户画像数据，提供更加个性化的回答。
-- **选择分析文本的数量**：允许您根据需要调整分析的深度和广度。           
-- **设置生成文本的创造性**：值越低，生成的文本越倾向于保守和预测性高；值越高，文本则越新颖和多样化。
+1. **个性化查询**：使用者可以基于用户的特定维度（如心理状态、兴趣爱好）提出查询，系统将提供相关的详细信息。
+2. **选择分析文本的数量**：允许您根据需要调整从向量数据库中取到的文本数量。           
+3. **设置生成文本的创造性**：值越低，生成的文本越倾向于保守和预测性高；值越高，文本则越新颖和多样化。
 
 ##### 使用步骤
 1. **问前准备**：首先，请确定您在Analysis模块以及进行分析。
 2. **提交查询**：通过输入框提交您的具体询问或探索要点，例如“该用户最感兴趣的活动是什么？”
 3. **查看回答**：系统将基于LangChain框架和LLM技术分析您的查询，并提供详细且个性化的回答。
-
-##### 最佳实践
-- **明确查询目的**：在提问前，明确您希望了解的用户维度，有助于系统提供更精准的信息。
-- **多角度探索**：鼓励从不同角度和维度进行用户画像的探索，以获得全面的用户理解。
 """
 )
 
@@ -49,12 +40,12 @@ index_name = "echopersona"
 
 number = st.slider('选择分析文本的数量', min_value=3, max_value=10, value=5, step=1)
 temperature = st.slider('设置生成文本的创造性', min_value=0.0, max_value=1.0, value=0.5, step=0.1)
-namespace = st.text_input("输入命名空间", value="hu")
+namespace = st.text_input("输入命名空间（选定的用户）", value="hu")
 
 if 'docsearch' not in st.session_state:
     st.session_state.docsearch = None
 
-query = st.text_input("Enter your query")
+query = st.text_input("输入对该用户的提问")
 embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
 keywords_chain = chains.ListStrChain(openai_api_key=openai_api_key, p_text=prompts.get_keywords_prompt())
 summary_chain = chains.CommonChain(openai_api_key=openai_api_key, p_text=prompts.get_summary_prompt(), temperature=temperature)
@@ -77,18 +68,18 @@ if st.button("Submit") and namespace:
             # Save uploaded file temporarily to disk, load and split the file into pages, delete temp file
             index = pc.Index(index_name)
 
-            docsearch = Pinecone.from_texts([], embeddings, index_name = index_name,namespace=namespace)
+            docsearch = Pinecone.from_texts([], embeddings, index_name = index_name, namespace=namespace)
 
-            keywords_list = keywords_chain.run(query=query)
-            keywords_str = ' '.join(keywords_list)
-            st.write("关键词：")
-            st.write(keywords_str)
+            # keywords_list = keywords_chain.run(query=query)
+            # keywords_str = ' '.join(keywords_list)
+            # st.write("关键词：")
+            # st.write(keywords_str)
 
-            docs = docsearch.similarity_search(keywords_str, k=number)
-
-            docs_str = "用户发言：" + '\n'.join([t.page_content for t in docs])
+            docs = docsearch.similarity_search(query, k=number)
+            print(docs)
+            docs_str = "根据这些用户发言，回答提问：" + '\n'.join([t.page_content for t in docs])
             q = "提问：" + query + "\n" + docs_str
-            res = summary_chain.run(query=docs_str)
+            res = summary_chain.run(query=q)
             print(res)
             st.write(res)
         except Exception as e:
